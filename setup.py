92bbdae29bf7f0c87a8a6ab2d32d40622ed9504b
Alexx96 <xrys-team@mail.ru>
2017-01-09 18:41:19 +0200

    BOB

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='eveme',
+    version='',
+    packages=['eveme', 'event', 'event.migrations', 'account', 'account.migrations'],
+    url='',
+    license='',
+    author='alex',
+    author_email='',
+    description=''
+)
